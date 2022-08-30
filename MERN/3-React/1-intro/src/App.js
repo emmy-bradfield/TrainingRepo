d94076d01cf52f791450d2c91257e96abd12c6fa
Emily Bradfield <emily-bradfield@outlook.com>
2022-08-30 09:14:42 +0100

    started 3-React

---
+++ MERN/3-React/1-intro/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2 className="title"> Introduction to React JS</h2>
+      </header>
+
+      <main className="App-main">
+        <h2> Lesson One: Intro </h2>
+
+      </main>
+
+      <footer className="App-footer">
+        <em> Created by Emily Bradfield as part of my training with QA - check out my <a href="https://www.github.com/emmy-bradfield" target="_blank">GitHub</a> for more</em>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
--- MachineLearning/1-Starter/e1-regression.py
+++ MachineLearning/1-Starter/e1-regression.py
@@ -5,3 +5,16 @@ import pandas as pd
 import numpy as np 
 from statistics import mean
 
+df = pd.DataFrame(pd.read_csv("1-Starter/assets/movieratings.csv"))
+Darray = df.to_numpy()
+x1_D = []
+x2_D = []
+y_D= []
+predicted_csv = []
+sq_error_csv = []
+
+for (x1, x2, y) in Darray:
+    x1_D.append(x1)
+    x2_D.append(x2)
+    y_D.append(y)
+
