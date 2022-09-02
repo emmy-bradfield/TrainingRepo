f6728cfe9ab4135bccd1d55c713b4c311031b02a
Emily Bradfield <emily-bradfield@outlook.com>
2022-09-02 08:50:48 +0100

    5-routing created and set up

---
+++ MERN/3-React/5-routing/src/components/main.jsx
@@ -0,0 +1,95 @@
+
+const Main = () => {
+
+    return (
+        <div className="App-main">
+            <h1> 5. React Routing and HTTP Requests</h1>
+            <br></br>
+            <h2>5.1.1 React Routing </h2>
+            <h4>Create a page which contains:</h4>
+            <ul className="stack">
+                <li> 1. Home page </li>
+                <li> 2. Users Page </li>
+                <li> 3 .Contact Page </li>
+                <li> 4. 404 page </li>
+            </ul>
+            <br></br>
+            <div className="output" height="10vh;">
+
+            </div>
+
+            <hr></hr>
+
+            <h2>5.1.2 React Routing </h2>
+            <h4>Create a switch and route for each of the pages in App.js
+            </h4>
+            <br></br>
+            <div className="output" height="10vh;">
+
+            </div>
+
+            <hr></hr>
+
+            <h2>5.1.3 React Routing </h2>
+            <h4>Create a URL parameter for users/:id - Render their id on the page
+            </h4>
+            <br></br>
+            <div className="output" height="10vh;">
+
+            </div>
+
+            <hr></hr>
+
+            <h2>5.1.4 React Routing </h2>
+            <h4> Implement nested routing within the User's component
+            </h4>
+            <br></br>
+            <div className="output" height="10vh;">
+
+            </div>
+            <hr></hr>
+
+            <h2>5.2 Data Requests </h2>
+            <h4> In this exercise, you are tasked with creating a movie search application.
+            </h4>
+            <ul className="stack">
+                <li>
+                    1. Navigate to OMDBAPI
+                </li>
+                <li>
+                    2. Get a free API key from the API Key tab
+                </li>
+                <li>
+                    3. Create a FilmRequest Component that lets <br />the user enter a Movie title and axios 
+                    fetches the <br /> correct movie from OMDBAPI.
+                </li>
+                <em> Try and use Parent/Child Components to present your data </em>
+            </ul>
+            <br></br>
+            <div className="output" height="10vh;">
+
+            </div>
+
+            <h2>5.3 Static Data </h2>
+            <h4> Create a component LoginControl which displays:
+            </h4>
+            <ul className="stack">
+                <li>
+                    1. Header with "Please sign up" as text and a Login Button
+                </li>
+                <li>
+                    2. Header with "Welcome Back!" as text and a Logout Button
+                </li>
+                <li>
+                    3. Onclick on each of the buttons should take the user between the two displays above.
+                </li>
+            </ul>
+            <br></br>
+            <div className="output" height="10vh;">
+
+            </div>
+        </div>
+    );
+}
+
+export default Main; 
