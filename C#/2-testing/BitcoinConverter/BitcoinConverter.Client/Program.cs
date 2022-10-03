ca943f7fd0b97e9ecbb7b0a81674b2cf72b945a6
Emily Bradfield <emily-bradfield@outlook.com>
2022-10-03 14:01:51 +0100

    client added

---
+++ C#/2-testing/BitcoinConverter/BitcoinConverter.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bitcoin;
+
+namespace Bitcoin.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var converter = new ConvertSvc();
+
+            while(true)
+            {
+                try
+                {
+                    Console.Write("Coins: ");
+                    var coins = Double.Parse(Console.ReadLine());
+                    Console.Write("Currency (USD, GBP, or EUR): ");
+                    var currency = (ConvertSvc.Currency)Enum.Parse(typeof(ConvertSvc.Currency), Console.ReadLine(), true);
+
+                    var amount = await converter.ConvertBitcoins(currency, coins);
+
+                    Console.WriteLine("{0} Bitcoins = {1} {2}", coins, amount, currency);
+                }
+                catch 
+                {
+                    //swallow
+                }
+            }
+        }
+    }
+}
